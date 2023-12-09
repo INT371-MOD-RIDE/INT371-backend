2cd703960223ca2d1ef1dce50d2cbc512408619e
sysadmin <varitchai.will@mail.kmutt.ac.th>
2023-12-09 10:16:56 +0700

    master commit

--- src/main/java/sit/int371/modride_service/SecurityConfigurer.java
+++ src/main/java/sit/int371/modride_service/SecurityConfigurer.java
@@ -66,6 +66,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/**").permitAll()
                 // friends
                 .antMatchers(HttpMethod.GET, "/api/v1/friends/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/v1/friends/**").permitAll()
@@ -78,6 +79,16 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.DELETE, "/api/v1/users/**").permitAll()
                 .antMatchers(HttpMethod.PUT, "/api/v1/users/**").permitAll()
                 .antMatchers(HttpMethod.OPTIONS, "/api/v1/users/**").permitAll()
+                .antMatchers(HttpMethod.GET, "http://capstone23.sit.kmutt.ac.th/un2/api/v1/users/getBranches").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/v1/users/getBranches").permitAll()
+                .antMatchers(HttpMethod.GET, "/un2/api/v1/users/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/un2/api/v1/users/**").permitAll()
+                .antMatchers(HttpMethod.DELETE, "/un2/api/v1/users/**").permitAll()
+                .antMatchers(HttpMethod.PUT, "/un2/api/v1/users/**").permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/un2/api/v1/users/**").permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/api/v1/users/getBranches").permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "http://capstone23.sit.kmutt.ac.th/un2/api/v1/users/getBranches").permitAll()
+
                 // events
                 .antMatchers(HttpMethod.GET, "/api/v1/events/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/v1/events/**").permitAll()
