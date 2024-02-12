d90bb430c7bfcfe178239992790a33f17288e4c8
ApiwatAt <apiwat.atittieng@mail.kmutt.ac.th>
2024-02-12 16:31:34 +0700

    validate comment

--- src/main/java/sit/int371/modride_service/beans/RatingBean.java
+++ src/main/java/sit/int371/modride_service/beans/RatingBean.java
@@ -1,11 +1,19 @@
 package sit.int371.modride_service.beans;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import lombok.Data;
 
 @Data
 public class RatingBean {
     private Integer rating_id;
+    // @NotBlank
+    // @NotNull
     private Integer user_id;
+    
+    // @NotBlank(message = "rating_point is required")
+    // @NotNull(message = "rating_point is required")
     private Integer rating_point;
     private Integer rating_amount;
 }
